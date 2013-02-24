6730ca5654ac941c9b910ef92af4f8e11598ce0e
Derek Willis <dwillis@gmail.com>
2013-02-24 17:10:57 -0600

    added a few more tests

--- test/test_document.rb
+++ test/test_document.rb
@@ -9,6 +9,7 @@ class TestDocument < MiniTest::Unit::TestCase
     @latest = Document.latest
     @categories = Category.all
     @locations = Document.with_locations
+    @doc = Document.detail('DCPD-201300104')
   end
 
   def test_that_there_are_five_latest_docs
@@ -19,6 +20,17 @@ class TestDocument < MiniTest::Unit::TestCase
     assert_nil @latest.first.lat
   end
   
+  def test_that_categories_have_a_positive_count
+    assert_operator @categories.count, :>=, 0
+  end
   
+  def test_that_location_docs_have_lat_and_lng
+    assert @locations.last.lat
+    assert @locations.first.lng
+  end
+  
+  def test_that_document_detail_has_an_fdsys_url
+    assert_equal "http://www.gpo.gov/fdsys/pkg/DCPD-201300104/html/DCPD-201300104.htm", @doc.fdsys_url
+  end
 
 end
